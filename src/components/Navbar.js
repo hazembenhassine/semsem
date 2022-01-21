32714aa321b9d6d0d92fc7427b0ca552b65eccae
Khaled Ghamgui <gham.khaled@gmail.com>
2022-01-21 17:25:51 +0100

    Semsem MarketPlace

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,11 +7,10 @@ class Navbar extends Component {
       <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
         <a
           className="navbar-brand col-sm-3 col-md-2 mr-0"
-          href="http://www.dappuniversity.com/bootcamp"
-          target="_blank"
+          href="http://localhost:3000"
           rel="noopener noreferrer"
         >
-          Dapp University's Blockchain Marketplace
+          Semsem Blockchain Marketplace
         </a>
         <ul className="navbar-nav px-3">
           <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
